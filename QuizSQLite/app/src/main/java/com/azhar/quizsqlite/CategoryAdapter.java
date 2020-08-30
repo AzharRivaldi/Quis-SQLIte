d49f2e1ad85cd11e8459263abd6dd559c49cecf9
Azhar Rivaldi <46733833+AzharRivaldi@users.noreply.github.com>
2020-08-30 10:33:54 +0700

    Update CategoryAdapter.java

--- QuizSQLite/app/src/main/java/com/azhar/quizsqlite/CategoryAdapter.java
+++ QuizSQLite/app/src/main/java/com/azhar/quizsqlite/CategoryAdapter.java
@@ -2,15 +2,20 @@ package com.azhar.quizsqlite;
 
 import android.content.Context;
 import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.azhar.quizsqlite.CategoryItem;
+import com.azhar.quizsqlite.QuestionActivity;
+import com.azhar.quizsqlite.R;
 
 import java.util.ArrayList;
 
